f83ee7ff5236ce0bdb01e95b535180e7791e6277
Yug34 <yug.gjr@gmail.com>
2025-08-28 14:41:32 +0530

    remove debugger -_-

--- src/components/Canvas.tsx
+++ src/components/Canvas.tsx
@@ -24,8 +24,6 @@ const Canvas = () => {
         const re =
           /^https?:\/\/(?:www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b(?:[-a-zA-Z0-9()@:%_.~#?&//=]*)$/;
 
-        debugger;
-
         if (text) {
           if (re.test(text)) {
             const webPageNode: CustomNode = {
