69ac8bb82ee28ef97187c33194613d76eca0eaf2
Yug34 <yug.gjr@gmail.com>
2025-09-10 02:58:08 +0530

    page counter and inc/dec

--- src/components/nodes/PDFNode.tsx
+++ src/components/nodes/PDFNode.tsx
@@ -4,6 +4,7 @@ import { Card } from "../ui/card";
 import { Document, Page, pdfjs } from "react-pdf";
 import { useRef, useState } from "react";
 import type { PDFDocumentProxy } from "pdfjs-dist";
+import { Button } from "../ui/button";
 
 pdfjs.GlobalWorkerOptions.workerSrc = new URL(
   "pdfjs-dist/build/pdf.worker.min.mjs",
@@ -13,7 +14,17 @@ pdfjs.GlobalWorkerOptions.workerSrc = new URL(
 export function PDFNode(NodeData: PDFNodeData) {
   const { data } = NodeData;
   const [numPages, setNumPages] = useState(0);
+  const [currentPage, setCurrentPage] = useState(1);
+
   const containerRef = useRef<HTMLDivElement>(null);
+
+  const incPage = () => {
+    setCurrentPage((prev) => Math.min(prev + 1, numPages));
+  };
+  const decPage = () => {
+    setCurrentPage((prev) => Math.max(prev - 1, 1));
+  };
+
   const onDocumentLoadSuccess = ({
     numPages: nextNumPages,
   }: PDFDocumentProxy): void => {
@@ -22,6 +33,10 @@ export function PDFNode(NodeData: PDFNodeData) {
 
   const MAX_WIDTH = 800;
 
+  const onPDFClick = (e: MouseEvent) => {
+    console.log("clicked");
+  };
+
   return (
     <Card className="p-4">
       <div className="text-sm font-medium">{data.fileName}</div>
@@ -29,21 +44,30 @@ export function PDFNode(NodeData: PDFNodeData) {
         className="flex flex-col items-center justify-center"
         ref={containerRef}
       >
-        <Document file={data.pdfBase64} onLoadSuccess={onDocumentLoadSuccess}>
-          {Array.from(new Array(numPages), (_el, index) => (
-            <Page
-              key={`page_${index + 1}`}
-              renderAnnotationLayer={false}
-              renderTextLayer={false}
-              pageNumber={index + 1}
-              width={
-                containerRef.current?.clientWidth
-                  ? Math.min(containerRef.current?.clientWidth, MAX_WIDTH)
-                  : MAX_WIDTH
-              }
-            />
-          ))}
+        <Document
+          file={data.pdfBase64}
+          onLoadSuccess={onDocumentLoadSuccess}
+          onClick={onPDFClick}
+        >
+          <Page
+            renderAnnotationLayer={false}
+            renderTextLayer={false}
+            pageNumber={currentPage}
+            width={
+              containerRef.current?.clientWidth
+                ? Math.min(containerRef.current?.clientWidth, MAX_WIDTH)
+                : MAX_WIDTH
+            }
+          />
         </Document>
+        <div className="flex items-center gap-2">
+          <Button variant="outline" onClick={decPage}>
+            Previous
+          </Button>
+          <Button variant="outline" onClick={incPage}>
+            Next
+          </Button>
+        </div>
         <Handle type="source" position={Position.Top} />
         <Handle type="target" position={Position.Bottom} />
       </div>
