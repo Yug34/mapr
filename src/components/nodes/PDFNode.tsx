643bd0bc1444ab4db0169db4a802128d1a6075c8
Yug34 <yug.gjr@gmail.com>
2025-09-10 03:41:29 +0530

    modal for pdfs

--- src/components/nodes/PDFNode.tsx
+++ src/components/nodes/PDFNode.tsx
@@ -6,6 +6,13 @@ import { useRef, useState } from "react";
 import type { PDFDocumentProxy } from "pdfjs-dist";
 import { Button } from "../ui/button";
 import { Minus, Plus } from "lucide-react";
+import {
+  Dialog,
+  DialogContent,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "../ui/dialog";
 
 pdfjs.GlobalWorkerOptions.workerSrc = new URL(
   "pdfjs-dist/build/pdf.worker.min.mjs",
@@ -16,6 +23,7 @@ export function PDFNode(NodeData: PDFNodeData) {
   const { data } = NodeData;
   const [numPages, setNumPages] = useState(0);
   const [currentPage, setCurrentPage] = useState(1);
+  const [isModalOpen, setIsModalOpen] = useState(false);
 
   const containerRef = useRef<HTMLDivElement>(null);
 
@@ -35,57 +43,77 @@ export function PDFNode(NodeData: PDFNodeData) {
   const MAX_WIDTH = 800;
 
   const onPDFClick = (e: MouseEvent) => {
-    console.log("clicked");
-    window.open(data.pdfBlobUrl, "_blank");
+    setIsModalOpen(true);
+    // window.open(data.pdfBlobUrl, "_blank");
   };
 
   return (
-    <Card className="p-4">
-      <div className="text-sm font-medium">{data.fileName}</div>
-      <div
-        className="flex flex-col items-center justify-center"
-        ref={containerRef}
-      >
-        <Document
-          file={data.pdfBase64}
-          onLoadSuccess={onDocumentLoadSuccess}
-          onClick={onPDFClick}
+    <>
+      <Dialog modal open={isModalOpen} onOpenChange={setIsModalOpen}>
+        <DialogContent className="w-full h-full max-w-[90vw] max-h-[90vh] overflow-y-auto">
+          <DialogHeader>
+            <DialogTitle>{data.fileName}</DialogTitle>
+          </DialogHeader>
+          <Document file={data.pdfBase64} onLoadSuccess={() => {}}>
+            {Array.from(new Array(numPages), (_el, index) => (
+              <Page
+                key={`page_${index + 1}`}
+                renderAnnotationLayer={false}
+                renderTextLayer={false}
+                pageNumber={index + 1}
+              />
+            ))}
+          </Document>
+        </DialogContent>
+      </Dialog>
+      <Card className="p-4">
+        <div className="text-sm font-medium">{data.fileName}</div>
+        <div
+          className="flex flex-col items-center justify-center"
+          ref={containerRef}
         >
-          <Page
-            renderAnnotationLayer={false}
-            renderTextLayer={false}
-            pageNumber={currentPage}
-            width={
-              containerRef.current?.clientWidth
-                ? Math.min(containerRef.current?.clientWidth, MAX_WIDTH)
-                : MAX_WIDTH
-            }
-          />
-        </Document>
-        <div className="flex items-center text-sm font-medium">
-          <Button
-            size="icon"
-            className="cursor-pointer rounded-r-none"
-            onClick={decPage}
-            disabled={currentPage === 1}
+          <Document
+            className="cursor-pointer"
+            file={data.pdfBase64}
+            onLoadSuccess={onDocumentLoadSuccess}
+            onClick={onPDFClick}
           >
-            <Minus />
-          </Button>
-          <Button variant="secondary" className="rounded-none font-normal">
-            Page {currentPage} of {numPages}
-          </Button>
-          <Button
-            size="icon"
-            className="cursor-pointer rounded-l-none"
-            onClick={incPage}
-            disabled={currentPage === numPages}
-          >
-            <Plus />
-          </Button>
+            <Page
+              renderAnnotationLayer={false}
+              renderTextLayer={false}
+              pageNumber={currentPage}
+              width={
+                containerRef.current?.clientWidth
+                  ? Math.min(containerRef.current?.clientWidth, MAX_WIDTH)
+                  : MAX_WIDTH
+              }
+            />
+          </Document>
+          <div className="flex items-center text-sm font-medium">
+            <Button
+              size="icon"
+              className="cursor-pointer rounded-r-none"
+              onClick={decPage}
+              disabled={currentPage === 1}
+            >
+              <Minus />
+            </Button>
+            <Button variant="secondary" className="rounded-none font-normal">
+              Page {currentPage} of {numPages}
+            </Button>
+            <Button
+              size="icon"
+              className="cursor-pointer rounded-l-none"
+              onClick={incPage}
+              disabled={currentPage === numPages}
+            >
+              <Plus />
+            </Button>
+          </div>
+          <Handle type="source" position={Position.Top} />
+          <Handle type="target" position={Position.Bottom} />
         </div>
-        <Handle type="source" position={Position.Top} />
-        <Handle type="target" position={Position.Bottom} />
-      </div>
-    </Card>
+      </Card>
+    </>
   );
 }
